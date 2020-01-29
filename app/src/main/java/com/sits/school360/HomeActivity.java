45a91737f0ab332f67b0895e5039ba1eca5f2ce6
AllPlayer <ayushparida999@gmail.com>
2020-01-29 15:58:01 +0530

    test

--- app/src/main/java/com/sits/school360/HomeActivity.java
+++ app/src/main/java/com/sits/school360/HomeActivity.java
@@ -50,8 +50,6 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
