eb7eb7261a6611f2ba8d82bc4ac153eeb43de922
AllPlayer <ayushparida999@gmail.com>
2020-02-04 18:09:39 +0530

    minor changes in manifest

---
+++ app/src/main/java/com/sits/school360/ui/teachersDetails/MyTeachersActivity.java
@@ -0,0 +1,115 @@
+package com.sits.school360.ui.teachersDetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.sits.school360.R;
+import com.sits.school360.ui.teachersDetails.TeacherDetailsDataObject;
+import com.sits.school360.ui.teachersDetails.TeacherDetailsRecyclerViewAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MyTeachersActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private int x;
+    String URL = "http://apischools360.sitslive.com/Api/Fee?stuCode=931&key=@@schools@@@@@@@@@3@@@@&schoolCodeKey=3";
+    ArrayList<String> Date;
+    ArrayList<String> FeeFor;
+    ArrayList<Integer> TotalAmount;
+    ArrayList<Integer> TotalDue;
+    ArrayList<Integer> TotalReceive;
+    ArrayList<Integer> Balance;
+
+    private static String LOG_TAG = "CardViewActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_teachers);
+        Date = new ArrayList<>();
+        FeeFor = new ArrayList<>();
+        TotalAmount = new ArrayList<>();
+        TotalReceive = new ArrayList<>();
+        TotalDue = new ArrayList<>();
+        Balance = new ArrayList<>();
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        loadCardsData(URL);
+    }
+
+    private ArrayList<TeacherDetailsDataObject> getDataSet() {
+        ArrayList results = new ArrayList<TeacherDetailsDataObject>();
+        for (int index = 0; index < x; index++) {
+            TeacherDetailsDataObject obj = new TeacherDetailsDataObject(""/*+FeeFor.get(index).toString()*/,
+                    ""/*"Date: " + Date.get(index).toString()*/, "Opening Balance : " + TotalAmount.get(index), "Total Deposit: " +
+                    TotalReceive.get(index), "Total Due: " + TotalDue.get(index), "Balance: " + Balance.get(index));
+            results.add(index, obj);
+        }
+        return results;
+    }
+
+    private void loadCardsData(String url) {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                String[] res = new String[]{"a", response};
+                res[1] = "{\"name\":" + res[1] + "}";
+                try {
+                    JSONObject jsonObject = new JSONObject(res[1]);
+                    JSONArray jsonArray = jsonObject.getJSONArray("name");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                        String date = jsonObject1.getString("Date");
+                        String feeFor = jsonObject1.getString("FeeFor");
+                        Integer totalAmount = jsonObject1.getInt("TotalAmount");
+                        Integer totalDue = jsonObject1.getInt("TotalDue");
+                        Integer totalReceive = jsonObject1.getInt("TotalReceive");
+                        Integer balance = jsonObject1.getInt("Balance");
+                        Date.add(date);
+                        FeeFor.add(feeFor);
+                        TotalAmount.add(totalAmount);
+                        TotalDue.add(totalDue);
+                        TotalReceive.add(totalReceive);
+                        Balance.add(balance);
+                        x = x + 1;
+                    }
+
+                    mAdapter = new TeacherDetailsRecyclerViewAdapter(getDataSet());
+                    mRecyclerView.setAdapter(mAdapter);
+                    //spinner.setAdapter(new ArrayAdapter<String>(FeeSummaryActivity.this, android.R.layout.simple_spinner_dropdown_item, SchoolNames));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        int socketTimeout = 30000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+        requestQueue.add(stringRequest);
+    }
+}
