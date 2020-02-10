8a3b07efde6bb464b01189e696055ac99d4f7c21
AllPlayer <ayushparida999@gmail.com>
2020-02-10 12:21:53 +0530

    changing right menu

--- app/src/main/java/com/sits/school360/ui/fee/FeeFragment.java
+++ app/src/main/java/com/sits/school360/ui/fee/FeeFragment.java
@@ -1,5 +1,6 @@
 package com.sits.school360.ui.fee;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,28 +9,49 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.sits.school360.R;
+import com.sits.school360.ui.examSchedule.ExamScheduleActivity;
+import com.sits.school360.ui.feeDueDetails.FeeDueDetailsActivity;
+import com.sits.school360.ui.feePaidDetails.FeePaidDetailsActivity;
+import com.sits.school360.ui.feeSummary.FeeSummaryActivity;
 
 public class FeeFragment extends Fragment {
 
     private FeeViewModel feeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        feeViewModel =
-                ViewModelProviders.of(this).get(FeeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_attendance, container, false);
-        final TextView textView = root.findViewById(R.id.text_tools);
-        feeViewModel.getText().observe(this, new Observer<String>() {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        CardView personalDetails=v.findViewById(R.id.FeeSummary);
+        personalDetails.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FeeSummaryActivity.class);
+                getActivity().startActivity(intent);
             }
         });
-        return root;
+        CardView feeDueDetails=v.findViewById(R.id.FeeDueDetails);
+        feeDueDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FeeDueDetailsActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+        CardView examSchedule=v.findViewById(R.id.FeeReceiveDetails);
+        examSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FeePaidDetailsActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+        return v;
     }
 }
