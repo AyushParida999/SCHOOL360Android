dbe9b1193415427da75c5dc73153dc2a13f83023
Rahul Chaudhary <30838739+rahulchaudharydeveloper@users.noreply.github.com>
2020-02-10 12:34:06 +0530

    card view in fragment

---
+++ app/src/main/java/com/sits/school360/ui/notices/DataNoteImformation.java
@@ -0,0 +1,8 @@
+package com.sits.school360.ui.notices;
+
+public class DataNoteImformation {
+    public static String[] textArray = {"Lightshot 截圖工具"};
+    public static String[] dateArray = {"2017-04-25"};
+    public static String[] id = {"1"};
+
+}
--- app/src/main/java/com/sits/school360/ui/notices/NoticesFragment.java
+++ app/src/main/java/com/sits/school360/ui/notices/NoticesFragment.java
@@ -1,35 +1,230 @@
 package com.sits.school360.ui.notices;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.sits.school360.R;
+import com.sits.school360.ui.exam.ExamFragment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class NoticesFragment extends Fragment {
+    private ListAdapter mListadapter;
+    private RecyclerView mRecyclerView;
+    public static final String TAG = "MyTag";
+    RequestQueue requestQueue;
+    ArrayList<String> FeeFor;
+    public static String test="";
+    public NoticesFragment() {
+        // Required empty public constructor
+    }
 
-    private NoticesViewModel noticesViewModel;
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment Volley.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NoticesFragment newInstance() {
+        NoticesFragment fragment = new NoticesFragment();
+        Bundle args = new Bundle();
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        noticesViewModel =
-                ViewModelProviders.of(this).get(NoticesViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notices, container, false);
-        final TextView textView = root.findViewById(R.id.text_tools);
-        noticesViewModel.getText().observe(this, new Observer<String>() {
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_notices, container, false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.my_testcrdv_rec);
+        standardQueueStringRequest(v);
+
+
+
+
+
+
+        // Inflate the layout for this fragment
+        return v;
+    }
+
+    private void standardQueueStringRequest(View v) {
+        //final TextView mTextView = (TextView) v.findViewById(R.id.text_tools);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+       final ArrayList data = new ArrayList<DataNote>();
+
+        // StringRequest with VOLLEY with Standard RequestQueue
+        // Instantiate the RequestQueue.
+        requestQueue = Volley.newRequestQueue(v.getContext());
+        String url = "http://apischools360.sitslive.com/Api/Fee?stuCode=931&key=@@schools@@@@@@@@@3@@@@&schoolCodeKey=3";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        String[] res= new String[]{"a",response};
+                        res[1]="{\"name\":"+res[1]+"}";
+                        try{
+                            JSONObject jsonObject=new JSONObject(res[1]);
+                            JSONArray jsonArray=jsonObject.getJSONArray("name");
+                            for(int i=0;i<jsonArray.length();i++){
+                                JSONObject jsonObject1=jsonArray.getJSONObject(i);
+                                String date=jsonObject1.getString("Date");
+                                String feeFor=jsonObject1.getString("FeeFor");
+                                Integer totalAmount=jsonObject1.getInt("TotalAmount");
+                                Integer totalDue=jsonObject1.getInt("TotalDue");
+                                Integer totalReceive=jsonObject1.getInt("TotalReceive");
+                                Integer balance=jsonObject1.getInt("Balance");
+                                test=feeFor;
+
+                                    data.add(
+                                            new DataNote
+                                                    (
+                                                            date,
+                                                            feeFor,
+                                                            date
+                                                    ));
+
+
+                            }
+
+                            //spinner.setAdapter(new ArrayAdapter<String>(FeeSummaryActivity.this, android.R.layout.simple_spinner_dropdown_item, SchoolNames));
+                        }catch (JSONException e){e.printStackTrace();}
+                        // Display the first 500 characters of the response string.
+                        //mTextView.setText("Response is: " + test);
+                        mListadapter = new ListAdapter(data);
+                        mRecyclerView.setAdapter(mListadapter);
+
+                    }
+                }, new Response.ErrorListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onErrorResponse(VolleyError error) {
+                //mTextView.setText("That didn't work!");
             }
         });
-        return root;
+
+
+
+
+
+
+
+
+        stringRequest.setTag(TAG);
+        // Add the request to the RequestQueue.
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (requestQueue != null) {
+            requestQueue.cancelAll(TAG);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+    public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder>
+    {
+        private ArrayList<DataNote> dataList;
+
+        public ListAdapter(ArrayList<DataNote> data)
+        {
+            this.dataList = data;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder
+        {
+            TextView textViewText;
+            TextView textViewComment;
+            TextView textViewDate;
+
+            public ViewHolder(View itemView)
+            {
+                super(itemView);
+               // this.textViewText = (TextView) itemView.findViewById(R.id.text);
+                this.textViewComment = (TextView) itemView.findViewById(R.id.text_teest);
+                //this.textViewDate = (TextView) itemView.findViewById(R.id.date);
+            }
+        }
+
+        @Override
+        public ListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.testcardv, parent, false);
+
+            ViewHolder viewHolder = new ViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ListAdapter.ViewHolder holder, final int position)
+        {
+            //holder.textViewText.setText(dataList.get(position).getText());
+            holder.textViewComment.setText(dataList.get(position).getComment());
+           // holder.textViewDate.setText(dataList.get(position).getDate());
+
+            holder.itemView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    Toast.makeText(getActivity(), "Item " + position + " is clicked.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount()
+        {
+            return dataList.size();
+        }
     }
 }
